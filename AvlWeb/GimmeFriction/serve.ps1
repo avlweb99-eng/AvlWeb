253611e0aff03abed56c187342f05967620b9594
avlweb99-eng <avlweb99@gmail.com>
2026-03-25 15:12:32 -0400

    Delete AvlWeb directory

--- AvlWeb/GimmeFriction/serve.ps1
+++
@@ -1,9 +0,0 @@
-param(
-  [int]$Port = 8000,
-  [string]$HostName = "127.0.0.1"
-)
-
-$ErrorActionPreference = "Stop"
-$scriptDir = Split-Path -Parent $MyInvocation.MyCommand.Path
-
-& node (Join-Path $scriptDir "serve.mjs") --port $Port --host $HostName
